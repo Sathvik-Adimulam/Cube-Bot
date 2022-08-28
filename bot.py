0fe909a714358d1f6661de4ce4862ecf5548e3bb
Sathvik-Adimulam <sathvik.adimulam@gmail.com>
2022-08-28 12:19:48 -0700

    as

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord 
 import dotenv 
 import os    
+import numpy as np 
 
 from discord.ext import commands 
 
@@ -17,6 +18,19 @@ class CubingBot(commands.Bot):
         @self.command()
         async def hi(ctx):
             await ctx.channel.send(f"Hi {ctx.author.display_name}!")
+            
+        @bot.command()
+        async def aon(ctx, *args, help='Calculates the aon of n solves'):
+            args = list(args)
+            for i, x in enumerate(args):
+                if str(x) == 'dnf':
+                    args[i] = np.inf
+                else:
+                    args[i] = float(x)
+            args.remove(max(args))
+            args.remove(min(args))
+            await ctx.channel.send(
+                f"{ctx.author.mention} has an ao{len(args)} of {np.mean(args)}")
 
     async def on_ready(self):
         print("--------------------------------------------------")
